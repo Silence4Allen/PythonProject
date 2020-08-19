c2664ecce9ec5f381a9b2432aebb4302deda2080
Allen <501983707@qq.com>
2020-08-19 20:32:38 +0800

    单例模式

---
+++ design_patterns/singleton.py
@@ -0,0 +1,10 @@
+"""
+单例模式
+"""
+
+
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
