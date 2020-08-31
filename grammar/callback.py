f3db97b48554b70f85b69a1e58fd1575bd807557
Allen <501983707@qq.com>
2020-08-31 20:10:09 +0800

    回调函数

---
+++ grammar/callback.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:         callback
+# Description:  回调函数https://www.zhihu.com/question/19801131
+# Author:       Allen
+# Time:         2020/8/31 20:01
+# -------------------------------------------------------------------------------
+
+
+class Transport:
+    def bicycle(self, goods):
+        print(f'this is a bicycle with {goods}')
+
+    def car(self, goods):
+        print(f'this is a car with {goods}')
+
+    def airplane(self, goods):
+        print(f'this is a airplane with {goods}')
+
+
+if __name__ == '__main__':
+    t = Transport()
+
+
+    def seller(transport_method, goods):
+        print(f'this is seller, and using {transport_method.__name__} to transfer {goods}')
+        transport_method(goods)
+
+
+    seller(t.car, '鞋子')
